ba3c743bef3e3c46e51a900f4049e94dc733f103
Raj <shahriarraj121@gmail.com>
2022-08-10 03:42:38 +0600

    With About

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='foot'>
+            <div className='abrar'>
+                <h1 >Abrar Mahmud</h1>
+            </div>
+            <div className='raj'>
+                <h1>Shahriar Raj</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
